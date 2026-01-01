6a3701bff6780a6c7e42242a996d00657411da0f
Maharudra Yadav <88700864+maharudrayadav@users.noreply.github.com>
2026-01-01 22:43:36 +0530

    Update WebConfig.java

--- resumeBuider/src/main/java/resumeBuider/config/WebConfig.java
+++ resumeBuider/src/main/java/resumeBuider/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all paths
-                .allowedOrigins("http://localhost:5173") // Your Vite/React URL
+               .allowedOriginPatterns("*") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
