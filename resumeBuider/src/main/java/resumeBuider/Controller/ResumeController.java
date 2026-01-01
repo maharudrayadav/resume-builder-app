2028486ed9b1a30677bd68a33b31e834fb07280f
Maharudra Yadav <88700864+maharudrayadav@users.noreply.github.com>
2026-01-01 22:29:42 +0530

    Update ResumeController.java

--- resumeBuider/src/main/java/resumeBuider/Controller/ResumeController.java
+++ resumeBuider/src/main/java/resumeBuider/Controller/ResumeController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 
 @RestController
 @RequestMapping("/api/resumes")
